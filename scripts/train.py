e43f1af8faac2b973c852597a633e10a9d5821b6
Carl <cboettig@gmail.com>
2024-02-14 21:38:14 +0000

    script

--- scripts/train.py
+++ scripts/train.py
@@ -4,7 +4,7 @@ parser = argparse.ArgumentParser()
 parser.add_argument("-f", "--file", help="Path config file", type=str)
 args = parser.parse_args()
 
-from gymnasium.envs.registration import register
+from rl4fisheries import Asm, Asm2o
 
 from rl4eco.utils import sb3_train    
 sb3_train(args.file)
--- src/rl4fisheries/__init__.py
+++ src/rl4fisheries/__init__.py
@@ -1,5 +1,7 @@
 from rl4fisheries.envs.asm import Asm
 from rl4fisheries.envs.asm_2o import Asm2o
+from rl4fisheries.envs.asm_esc import AsmEsc
+
 from rl4fisheries.agents.cautionary_rule import CautionaryRule
 from rl4fisheries.agents.const_esc import ConstEsc
 from rl4fisheries.agents.msy import Msy
