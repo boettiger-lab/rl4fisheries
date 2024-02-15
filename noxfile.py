c7dc68757d7caa74494d32036c3e1502a86c0a9a
Carl <cboettig@gmail.com>
2024-02-15 04:38:21 +0000

    keep it simple

--- noxfile.py
+++ noxfile.py
@@ -3,7 +3,7 @@ import nox
 
 # For this to run you will need to have python3.9, python3.10 and python3.11 installed on your computer. Otherwise nox will skip running tests for whatever versions are missing
 
-@nox.session(python=["3.9", "3.10", "3.11", "3.12"])
+@nox.session(python=["3.10", "3.11"])
 def test(session):
 
     # install
