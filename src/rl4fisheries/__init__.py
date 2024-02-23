808eb4eaaa4b816a43368c0f4706b9baadb9922f
Carl Boettiger <cboettig@gmail.com>
2024-02-23 18:41:32 +0000

    tidy

--- src/rl4fisheries/__init__.py
+++ src/rl4fisheries/__init__.py
@@ -1,8 +1,11 @@
+# Importing from sub-directories here makes these available as 'top-level' imports
 from rl4fisheries.envs.asm import Asm
 from rl4fisheries.envs.asm_2o import Asm2o
 from rl4fisheries.envs.asm_esc import AsmEsc
+
 from rl4fisheries.agents.cautionary_rule import CautionaryRule
 from rl4fisheries.agents.const_esc import ConstEsc
 from rl4fisheries.agents.msy import Msy
-from rl4fisheries.agents.unit_interface import unitInterface
 
+from rl4fisheries.utils.evaluation import gen_ep_rew, gather_stats
+from rl4fisheries.utils.sb3 import load_sb3_agent
