654fe8d41e554f6b9216b5211999d64e6db22bfb
Carl Boettiger <cboettig@gmail.com>
2024-02-25 20:00:59 +0000

    scikit opt results

--- src/rl4fisheries/__init__.py
+++ src/rl4fisheries/__init__.py
@@ -6,4 +6,3 @@ from rl4fisheries.envs.asm_esc import AsmEsc
 from rl4fisheries.agents.cautionary_rule import CautionaryRule
 from rl4fisheries.agents.const_esc import ConstEsc
 from rl4fisheries.agents.msy import Msy
-
