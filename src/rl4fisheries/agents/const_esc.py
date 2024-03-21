d76eddd1ae3741cd92d1c54513cef6fdc19ab59f
Felipe Montealegre-Mora <felimomo@berkeley.edu>
2024-03-21 02:13:30 +0000

    evaluate_policy uses vec envs of len=1, patched the bug that arose from that

--- src/rl4fisheries/agents/const_esc.py
+++ src/rl4fisheries/agents/const_esc.py
@@ -5,15 +5,18 @@ import polars as pl
 from tqdm import tqdm
 
 class ConstEsc:
-    def __init__(self, escapement=0, bounds = 1, **kwargs):
+    def __init__(self, env, escapement=0, bounds = 1, **kwargs):
         from .unit_interface import unitInterface
         self.ui = unitInterface(bounds=bounds)
         self.escapement = escapement
         self.bounds = bounds
         self.policy_type = "constant_escapement"
+        self.env = env
 
 
     def predict(self, observation, **kwargs):
+        if isVecObs(observation, self.env):
+            observation = observation[0]
         pop = self.ui.to_natural_units(observation)
         raw_prediction = self.predict_raw(pop)
         return 2 * raw_prediction - 1, {}
