24f4d54088367e7c6372a86dbecb1c6d6252291c
Felipe Montealegre-Mora <felimomo@berkeley.edu>
2024-07-01 22:34:01 +0000

    CR test debug

--- tests/test_agents.py
+++ tests/test_agents.py
@@ -1,8 +1,42 @@
 from rl4fisheries import CautionaryRule, Msy, ConstEscapement, AsmEnv
 
+#
+# Cautionary rule:
+
 def test_CR_biomass():
     env = AsmEnv()
     agent = CautionaryRule(env=env, x1=0, x2=env.bound, y2=1, observed_var='biomass')
+
+    assert (
+            (agent.x1_pm1 == -1) and
+            (agent.x2_pm1 == +1) and
+            (agent.y2_pm1 == +1)
+    ), "CR agent: Conversion of policy to [-1,+1] space lead to inconsistencies."
+
+    pred1, info1 = agent.predict(observation=-1)[0]
+    pred2, info2 = agent.predict(observation=+1)
+
+    (
+        assert (
+            (pred1 == -1) and
+            (pred2 == +1) and
+        ),
+        "CR agent: agent.predict does not return expected prediction."
+    )
+    (
+        assert (isinstance(info1, dict)) and (isinstance(info2, dict)),
+        "CR agent: agent.predict returns a non-dict info."
+    )
+
+def test_CR_mwt():
+    env = AsmEnv()
+    agent = CautionaryRule(
+        env=env, 
+        x1=env.parameters["min_wt"], 
+        x2=env.parameters["max_wt"], 
+        y2=1, 
+        observed_var='mean_wt',
+    )
 
     (
         assert (
@@ -19,9 +53,9 @@ def test_CR_biomass():
     (
         assert (
             (pred1 == -1) and
-            (pred == +1) and
+            (pred2 == +1) and
         ),
-        "CR agent: agent.predict does note return expected prediction."
+        "CR agent: agent.predict does not return expected prediction."
     )
     (
         assert (isinstance(info1, dict)) and (isinstance(info2, dict)),
